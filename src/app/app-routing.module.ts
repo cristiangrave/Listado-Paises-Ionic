19bcfbd6e57c728e703f9fa527137e392ec0ba7b
cristian Grave <cristiangrave002@gmail.com>
2024-11-25 20:08:52 -0600

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+/*aqui se conectan nuestros modules es lo primero que debemos de hacer a la hora de crear un nuevo componente */
 const routes: Routes = [
   {
     path: '',
@@ -11,6 +12,11 @@ const routes: Routes = [
     path: 'folder/:id',
     loadChildren: () =>
       import('./folder/folder.module').then((m) => m.FolderPageModule),
+  },
+  {
+    path: 'paises',
+    loadChildren: () =>
+      import('./listado/listado.module').then((m) => m.ListadoModule),
   },
 ];
 
--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -6,15 +6,10 @@ import { IonicModule } from '@ionic/angular';
 
 import { FolderPageRoutingModule } from './folder-routing.module';
 
-import { FolderPage } from './folder.page';
+import { FolderPage } from './folder.page'; /* archivo html redireccionado */
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    FolderPageRoutingModule
-  ],
-  declarations: [FolderPage]
+  imports: [CommonModule, FormsModule, IonicModule, FolderPageRoutingModule],
+  declarations: [FolderPage],
 })
 export class FolderPageModule {}
